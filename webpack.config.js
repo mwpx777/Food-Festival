0366cf63369c0ed2b3d688b6b701abb00a5d8459
MWPX777 <mwpx777@gmail.com>
2021-04-18 08:27:54 -0500

    created js bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 const path = require('path');
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 
 module.exports = {
-    entry: './assets/js/script.js',
+    // these are entry points for all of the js files
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     output: {
-        // path will add folder to create main.bundle.js
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        // build will create series of bundled files from entry objects
+        // example app.bundle.js, events.bundle.js
+        // output bundle files will be written to dist folder
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist'
     },
      plugins:[
         new webpack.ProvidePlugin({
           $: "jquery",
           jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", //the report outputs to an HTML file in the dist folder
+        })
       ],
     mode: 'development'
 };
